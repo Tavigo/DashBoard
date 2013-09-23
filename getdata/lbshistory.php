d5c9b03b5b4e0b05d35ebef384787d97ec011a86
tavi <gemmatavi2001!gi>
2013-09-23 12:18:42 +0200

    No son necesarias estas dos rutinas; utilizamos la función Coleccion de rutinas.php 	deleted:    lbsRSSIavg.php 	deleted:    lbshistory.php

--- getdata/lbshistory.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// conectar a la base de datos
-	$m = new MongoClient("5.56.58.76");
-	// seleccionar una base de datos
-	$db = $m->lbs;
-	// seleccionar una colección (equivalente a una tabla en una base de datos relacional)
-	$collection = $db->lbshistory;
-?>
